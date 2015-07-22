ed76e9d545586b50e70224686d0163669696eb40
Jason D. G <JDay18Gi@users.noreply.github.com>
2015-07-21 22:25:09 -0400

    add basic google map

--- gen2JS/gen2app.js
+++ gen2JS/gen2app.js
@@ -1,8 +1,21 @@
 $(document).ready(function() {
+
+	// For the Width of the info-section
 	console.log("Hi there! This website was created by createhard.com  Hope you like it!");
 	$("#info-bar").css("width", $("body img").width());
 	$(window).resize(function(){
 		$("#info-bar").css("width", $("body img").width());
 	});
 
+	// Google Map on #map-page
+	function initialize() {
+		var mapOptions = {
+			center: { lat: -34.397, lng: 150.644},
+			zoom: 8
+		};
+
+		var map = new google.maps.Map(document.getElementById('google-map'),
+		mapOptions);
+		}
+		google.maps.event.addDomListener(window, 'load', initialize);
 });
